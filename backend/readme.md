31bc77eff0dbce2ca2f81551d2cf8adace2ed636
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2025-01-09 15:51:04 +0530

    login API

--- backend/readme.md
+++ backend/readme.md
@@ -25,7 +25,8 @@ qrcode@^1.5.4 \
 resend@^4.0.1-alpha.0 \
 speakeasy@^2.0.0 \
 zod@^3.23.8 \
-uuid@^11.0.4
+uuid@^11.0.4 \
+date-fns@^4.1.0
 ```
 
 ### Install Dev Dependencies
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,10 +1,13 @@
 import { ErrorCode } from '../../common/enums/error-code.enum';
 import { VerificationEnum } from '../../common/enums/verification-code.enum';
-import { RegisterDto } from '../../common/interface/auth.interface';
+import { LoginDto, RegisterDto } from '../../common/interface/auth.interface';
 import { BadRequestException } from '../../common/utils/catch-errors';
 import { fortyFiveMinutesFromNow } from '../../common/utils/date-time';
+import { config } from '../../config/app.config';
+import SessionModel from '../../database/models/session.model';
 import UserModel from '../../database/models/user.model';
 import VerificationCodeModel from '../../database/models/verification.model';
+import jwt from 'jsonwebtoken';
 
 export class AuthService {
   public async register(registerData: RegisterDto) {
@@ -12,15 +15,18 @@ export class AuthService {
 
     const existingUser = await UserModel.exists({ email });
 
-    if(existingUser) {
-      throw new BadRequestException('User already exists with this email', ErrorCode.AUTH_EMAIL_ALREADY_EXISTS);
+    if (existingUser) {
+      throw new BadRequestException(
+        'User already exists with this email',
+        ErrorCode.AUTH_EMAIL_ALREADY_EXISTS
+      );
     }
 
     const newUser = await UserModel.create({
       name,
       email,
-      password
-    })
+      password,
+    });
 
     const userId = newUser._id;
 
@@ -28,12 +34,66 @@ export class AuthService {
       userId,
       type: VerificationEnum.EMAIL_VERIFICATION,
       expiresAt: fortyFiveMinutesFromNow(),
-    })
+    });
 
     // TODO: send email with verification code
 
     return {
-      user: newUser
+      user: newUser,
+    };
+  }
+
+  public async login(loginData: LoginDto) {
+    const { email, password, userAgent } = loginData;
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      throw new BadRequestException(
+        'Invalid email provided',
+        ErrorCode.AUTH_USER_NOT_FOUND
+      );
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      throw new BadRequestException(
+        'Invalid password provided',
+        ErrorCode.AUTH_USER_NOT_FOUND
+      );
+    }
+
+    // TODO: Check if the user enabled 2fa return user = null
+
+    const session = await SessionModel.create({
+      userId: user._id,
+      userAgent,
+    });
+
+    const accessToken = jwt.sign(
+      { userId: user._id, sessionId: session._id },
+      config.JWT.SECRET,
+      {
+        audience: ['user'],
+        expiresIn: config.JWT.EXPIRES_IN,
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      { sessionId: session._id },
+      config.JWT.REFRESH_SECRET,
+      {
+        audience: ['user'],
+        expiresIn: config.JWT.REFRESH_EXPIRES_IN,
+      }
+    );
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+      mfaRequired: false
     }
   }
 }
